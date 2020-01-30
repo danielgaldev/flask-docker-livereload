5842a99db62269d7ccb95071ba9bc072b4359304
Daniel Gal <daniel.gal@sch.bme.hu>
2020-01-30 02:20:19 +0100

    Success

--- src/app.py
+++ src/app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from livereload import Server
 
-def main():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def root():
-        return "Change"
-
-    server = Server(app.wsgi_app)
-    server.serve(port=5000, host='0.0.0.0')
-
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def root():
+    return "Awesome"
