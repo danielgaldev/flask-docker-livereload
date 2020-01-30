1144c8f3876a2300750847b31a5d2a82f4416209
Daniel Gal <daniel.gal@sch.bme.hu>
2020-01-30 01:57:54 +0100

    Not working yet

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export FLASK_APP=src/app.py
-export FLASK_ENV=development
-python src/manage.py run -h 0.0.0.0 -p 5000
--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from livereload import Server
 
-app = Flask(__name__)
+def main():
+    app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return "Hello"
+    @app.route("/")
+    def root():
+        return "Change"
+
+    server = Server(app.wsgi_app)
+    server.serve(port=5000, host='0.0.0.0')
+
+if __name__ == "__main__":
+    main()
