0626494a3477868b62b21e9e83c4a979e5b2d404
Daniel Gal <daniel.gal@sch.bme.hu>
2020-02-07 20:01:38 +0100

    Fixes, json printing

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    return render_template("index.html", text="Are u working?")
+    things = ["list item" for i in range(0, 10)]
+    json_content = {"name": "I am a text", "value": 12, "things": things}
+    content = json.dumps(json_content, indent=4)
+    return render_template("index.html", text=content)
